dca96633381792097b13f6df86556dfced5eba70
chockalingam subbiah <chockalingamsubbiah@chockalingams-MacBook-Pro.local>
2023-03-25 13:58:09 +0530

    commit 1

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+    "div", {id: "parent"}, React.createElement(
+        "div", {id: "child"}, [React.createElement(
+            "h1", {}, "I'm a h1 tag"
+        ), React.createElement(
+            "h2", {}, "I'm a h2 tag"
+        )]
+    ));
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(heading);
